a75f7048db4c88f119610ca7cf6e6d268b3190dd
Jay Sabale <jay.sable311@gmail.com>
2021-09-15 07:33:13 +0530

    npm flash incorporated and some code cleaned up

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
-const app = express()
-const port = 3000
 const path = require('path')
 const mongoose = require('mongoose')
-const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
-const catchAsync = require('./utils/catchAsync')
-const ExpressError = require('./utils/ExpressError')
 const session = require('express-session')
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError')
+const methodOverride = require('method-override')
+const passport= require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
+// routes
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
+const users = require('./routes/users') 
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     // useNewUrlParser: true,
@@ -24,16 +28,15 @@ db.once('open', () => {
     console.log('Database Connected!')
 });
 
+const app = express()
+
+app.engine('ejs', ejsMate)
 app.set('engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.engine('ejs', ejsMate)
+
 app.use(express.urlencoded({extended: true}))
-
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
     secret : 'thisshouldbeabettersecret',
@@ -45,11 +48,28 @@ const sessionConfig = {
         maxAge : 1000*60*60*24*7
     }
 }
-app.use(session(sessionConfig))
 
-app.listen(port, ()=>{
-    console.log('Serving on port 3000')
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
 })
+
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', users)
 
 app.get('/', (req, res)=> {
     res.render('home.ejs')
@@ -63,4 +83,8 @@ app.use((err, req, res, next) => {
     const { statusCode=500} = err;
     if(!err.message) err.message = 'Something Went Wrong'
     res.status(statusCode).render('./error.ejs', {err});
+})
+
+app.listen(port, ()=>{
+    console.log('Serving on port 3000')
 })
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router({mergeParams : true});
-const catchAsync = require('../utils/catchAsync')
-const ExpressError = require('../utils/ExpressError')
-const Campground = require('../models/campground')
-const Review = require('../models/review')
-const { campgroundSchema, reviewSchema } = require('../schemas.js')
+
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+
+const { campgroundSchema, reviewSchema } = require('../schemas.js');
+
+
+const ExpressError = require('../utils/ExpressError');
+const catchAsync = require('../utils/catchAsync');
 
 const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body)
-
+    const {error} = reviewSchema.validate(req.body);
     if(error){
         const msg = error.details.map(el => el.message).join(', ')
         throw(new ExpressError(msg, 400))
@@ -20,9 +23,10 @@ const validateReview = (req, res, next) => {
 router.post('/', validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
-    campground.reviews.push(review)
-    await campground.save()
-    await review.save()
+    campground.reviews.push(review);
+    await campground.save();
+    await review.save();
+    req.flash('success', 'Created new review!');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
@@ -30,9 +34,8 @@ router.delete('/:reviewId',  catchAsync( async (req, res) =>  {
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews : reviewId}});
     await Review.findByIdAndDelete(reviewId);
-
+    req.flash('success', 'Successfully deleted review')
     res.redirect(`/campgrounds/${id}`)
-
 }))
 
 module.exports = router;
